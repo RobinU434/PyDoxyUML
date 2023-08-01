15cad75733a323501e4d321c009bb422103f89e5
Robin Uhrich <robin.uhrich@gmail.com>
2023-08-01 14:37:17 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="pydoxyuml",
+    version="0.1",
+    description="PyDoxyUML - Collect Python code documentation and generate UML diagrams",
+    author="Robin Uhrich",
+    author_email="robin.uhrich@gmail.com",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "pydoxyuml = pydoxyuml.__main__:main",
+        ],
+    },
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",  # Updated license classifier
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    # Updated license information
+    license="Apache License, Version 2.0",
+)
